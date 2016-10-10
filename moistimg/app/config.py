c31c3c50f98a221e3ae98822889317404e19d862
dongwook.kim <dongwook.kim@rgpkorea.co.kr>
2016-10-10 13:25:34 +0900

    Add config file

---
+++ moistimg/app/config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+UPLOAD_FOLDER = '/img/uploads'
+
+ALLOWED_EXTENSION = ('png', 'jpg', 'jpeg', 'gif')
--- moistimg/manage.py
+++ moistimg/manage.py
@@ -8,6 +8,7 @@ from app import create_app
 
 
 app = create_app()
+app.config.from_object('app.config')
 # app.wsgi_app = ProxyFix(app.wsgi_app)
 manager = Manager(app)
 
